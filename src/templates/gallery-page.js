641368f360309dc6849f573f7c16f0afb8ed371d
matt <matt@atlasagency.com.au>
2018-01-17 16:54:01 +1100

    grid css

--- src/templates/gallery-page.js
+++ src/templates/gallery-page.js
@@ -9,111 +9,115 @@ import './grid.css'
 
 // export const GalleryTemplate = ({ content, contentComponent, description, title, hero, helmet, gallery }) => {
 class GalleryTemplate extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-			lightboxIsOpen: false,
-			currentImage: 0,
+      lightboxIsOpen: false,
+      currentImage: 0,
     };
-    
+
     this.closeLightbox = this.closeLightbox.bind(this);
-		this.gotoNext = this.gotoNext.bind(this);
-		this.gotoPrevious = this.gotoPrevious.bind(this);
-		this.gotoImage = this.gotoImage.bind(this);
-		this.handleClickImage = this.handleClickImage.bind(this);
-		this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+    this.gotoImage = this.gotoImage.bind(this);
+    this.handleClickImage = this.handleClickImage.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
   }
   // openLightbox(){
   // }
-  openLightbox (index, event) {
+  openLightbox(index, event) {
     // event.preventDefault();
     this.setState({
       currentImage: index,
       lightboxIsOpen: true,
     });
   }
-  closeLightbox () {
-		this.setState({
-			currentImage: 0,
-			lightboxIsOpen: false,
-		});
-	}
-	gotoPrevious () {
-		this.setState({
-			currentImage: this.state.currentImage - 1,
-		});
-	}
-	gotoNext () {
-		this.setState({
-			currentImage: this.state.currentImage + 1,
-		});
-	}
-	gotoImage (index) {
-		this.setState({
-			currentImage: index,
-		});
-	}
-	handleClickImage () {
-		if (this.state.currentImage === this.props.images.length - 1) return;
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+  gotoImage(index) {
+    this.setState({
+      currentImage: index,
+    });
+  }
+  handleClickImage() {
+    if (this.state.currentImage === this.props.images.length - 1) return;
 
-		this.gotoNext();
-	}
+    this.gotoNext();
+  }
   render() {
     let { content, contentComponent, description, title, hero, helmet, gallery } = this.props
     const PostContent = contentComponent || Content;
     // console.log(this.props)
     let LightboxImages = [];
-    gallery.forEach(g=>{
-      let temp = { src :g.node.childImageSharp.sizes.originalImg }
+    gallery.forEach(g => {
+      let temp;
+      if (g.node.childImageSharp) { temp = { src: g.node.childImageSharp.sizes.originalImg } }
       LightboxImages.push(temp);
     });
-    
+
     return <section className="section">
       {helmet ? helmet : ""}
       <div className="container content">
 
         <div className="columns">
           <div className="column is-6">
-            {gallery.length &&
-              <img src={hero} alt={title} />
-              || 
-              <Img sizes={gallery[0].node.childImageSharp.sizes} />
-            }
+            <div style={{ boxShadow: `5px 5px 0 0 black` }}>
+              {gallery.length &&
+                <img src={hero} alt={title} />
+                ||
+                <Img sizes={gallery[0].node.childImageSharp.sizes} />
+              }
+            </div>
           </div>
           <div className="column is-6">
-          <div className="" style={{ backgroundColor: '#fff', zIndex: 1, boxShadow: `10px 10px 0 0 black`,
-          padding:`1rem`, transform: `translate3d(-10%,20%,0)` }}>
-            <h1 className="title is-size-3 has-text-weight-bold is-bold-light">{title}</h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-          </div>
+            <div className="" style={{
+              backgroundColor: '#fff', zIndex: 1, boxShadow: `5px 5px 0 0 black,-5px -5px 0 0 black`,
+              padding: `1rem`, transform: `translate3d(-10%,20%,0)`
+            }}>
+              <h1 className="title is-size-3 has-text-weight-bold is-bold-light">{title}</h1>
+              {/* <p>{description}</p> */}
+              <PostContent content={content} />
+            </div>
           </div>
 
         </div>
-        {/* <div style={{
-          display: `grid`,
-          gridTemplateColumns: `repeat(auto-fill, minmax(300px, 1fr))`,
-          alignItems: `flex-start`,
-          gridGap: `10px`,
-          marginTop: `3rem`,
-        }}> */}
+
         <div className="gr">
-          {gallery.length && gallery.map((gal, i) =>
-            <div key={i} onClick={(e) => this.openLightbox(i, e)}>
-              <Img sizes={gal.node.childImageSharp.sizes} />
-            </div>
+          {gallery.length && gallery.map((gal, i) => {
+            if (gal.node.childImageSharp) {
+              return (
+                <div key={i} onClick={(e) => this.openLightbox(i, e)} style={{cursor: `pointer`}} className="hover">
+                  <Img sizes={gal.node.childImageSharp.sizes} />
+                </div>
+              )
+            }
+          }
           ) || <h2>Coming Soon!</h2>}
         </div>
         <Lightbox
-                currentImage={this.state.currentImage}
-                onClickThumbnail={this.gotoImage}
-                images={LightboxImages}
-                isOpen={this.state.lightboxIsOpen}
-                onClickPrev={this.gotoPrevious}
-                onClickNext={this.gotoNext}
-                onClose={this.closeLightbox}
-                showThumbnails={true}
-            />
+          currentImage={this.state.currentImage}
+          onClickThumbnail={this.gotoImage}
+          images={LightboxImages}
+          isOpen={this.state.lightboxIsOpen}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          onClose={this.closeLightbox}
+          showThumbnails={true}
+        />
 
       </div>
     </section>;
